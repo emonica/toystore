0cf4b418ee1915345adf151b1c8da2851d9fb0fd
Monica Galatanu <emonica@gmail.com>
2015-09-30 21:15:19 -0700

    Removed unused code causing errors with G+ signin

--- project.py
+++ project.py
@@ -362,8 +362,7 @@ def gconnect():
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    # Obtain authorization code
-    request.get_data()
+    # Obtain authorization code    
     code = request.data.decode('utf-8')
 
     try:
